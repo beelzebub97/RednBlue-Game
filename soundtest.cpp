abfec68fa2a51b1c6bc65c34a066e065df9a6a6a
Shivam Pathak <pathak.shivam97@gmail.com>
2019-04-08 00:59:49 +0530

    Testing & Adding Sound

---
+++ soundtest.cpp
@@ -0,0 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
+#include<stdio.h>
+#include<dos.h>
+
+void main()
+{
+     for(int i=1;i<10;i++)
+	   {
+	   sound(i*100);
+	   delay(100);
+	   }
+	   sound(i*50);
+	   delay(100);
+	   nosound();
+	   //cout<<"BEELZEBUB97" ;
+	   }
+	   getch();
+
+}
